b6723f299262d1b14e96874d64861802cd602736
Sourabh Kumar <sourabh.47512@gmail.com>
2024-08-11 21:39:45 +0530

    network call for delivery profile

---
+++ app/src/main/java/com/eggbucket/eggbucket_android/model/data/DeliveryProfile.kt
@@ -0,0 +1,11 @@
+package com.eggbucket.eggbucket_android.model.data
+
+data class DeliveryPartner(
+    val _id: String,
+    val firstName: String,
+    val lastName: String,
+    val driverLicenceNumber: String,
+    val phoneNumber: String,
+    val img: String,
+    val __v: Int
+)
--- app/src/main/java/com/eggbucket/eggbucket_android/network/ApiService.kt
+++ app/src/main/java/com/eggbucket/eggbucket_android/network/ApiService.kt
@@ -5,6 +5,7 @@ import com.eggbucket.eggbucket_android.model.DeliveryPartnersItem
 import com.eggbucket.eggbucket_android.model.Order
 import com.eggbucket.eggbucket_android.model.OrderCreate
 import com.eggbucket.eggbucket_android.model.VendorItem
+import com.eggbucket.eggbucket_android.model.data.DeliveryPartner
 import com.eggbucket.eggbucket_android.model.data.OutletPartnerResponse
 import retrofit2.Call
 import retrofit2.http.Body
@@ -15,6 +16,9 @@ import retrofit2.http.Path
 interface ApiService {
     @GET("outletPartners/egg-bucket-b2b/outlet_partner/{id}")
     fun getOutletPartner(@Path("id") id: String): Call<OutletPartnerResponse>
+
+    @GET("deliveryDrivers/egg-bucket-b2b/delivery_partner/{id}")
+    fun getDeliveryPartner(@Path("id") id: String): Call<DeliveryPartner>
 
     @GET("deliveryDrivers/egg-bucket-b2b/displayAll-delivery_partner")
     suspend fun getAllDeliveryPartners(): List<DeliveryPartnersItem>
