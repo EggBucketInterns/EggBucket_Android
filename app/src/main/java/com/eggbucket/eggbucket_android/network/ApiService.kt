fd20b196fe928a90a02a5a547fb1e565f949bfce
Shubham-344 <shubhamanand030404@gmail.com>
2024-08-13 15:00:39 +0530

    Resolved errors

--- app/src/main/java/com/eggbucket/eggbucket_android/network/ApiService.kt
+++ app/src/main/java/com/eggbucket/eggbucket_android/network/ApiService.kt
@@ -1,11 +1,16 @@
 package com.eggbucket.eggbucket_android.network
 
 import com.eggbucket.eggbucket_android.model.DeliveryPartnersItem
+import com.eggbucket.eggbucket_android.model.Order
+import com.eggbucket.eggbucket_android.model.OrderCreate
+import com.eggbucket.eggbucket_android.model.VendorItem
 import com.eggbucket.eggbucket_android.model.allorders.GetAllOrdersItem
 import com.eggbucket.eggbucket_android.model.data.DeliveryPartner
 import com.eggbucket.eggbucket_android.model.data.OutletPartnerResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ApiService {
@@ -17,6 +22,12 @@ interface ApiService {
 
     @GET("deliveryDrivers/egg-bucket-b2b/displayAll-delivery_partner")
     suspend fun getAllDeliveryPartners(): List<DeliveryPartnersItem>
+
+    @GET("vendors/egg-bucket-b2b/getAllVendor")
+    suspend fun getAllVendors():ArrayList<VendorItem>
+
+    @POST("orders/egg-bucket-b2b/create-order")
+    fun createOrder(@Body order: OrderCreate): Call<Void>
 
     @GET("orders/egg-bucket-b2b/getAllOrder")
     suspend fun getAllOrders():ArrayList<GetAllOrdersItem>
