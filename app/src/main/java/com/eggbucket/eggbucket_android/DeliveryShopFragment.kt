fbe6f7275ad0393bb7cc8193442f98904cdf92f6
Sourabh Kumar <sourabh.47512@gmail.com>
2024-08-27 15:58:39 +0530

    switched descending to ascending in all order

--- app/src/main/java/com/eggbucket/eggbucket_android/DeliveryShopFragment.kt
+++ app/src/main/java/com/eggbucket/eggbucket_android/DeliveryShopFragment.kt
@@ -2,15 +2,13 @@ package com.eggbucket.eggbucket_android
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.eggbucket.eggbucket_android.adapters.OrdersAdapter
+import com.eggbucket.eggbucket_android.adapters.OrderAdapter_2
 import com.eggbucket.eggbucket_android.model.allorders.GetAllOrdersItem
 import com.eggbucket.eggbucket_android.network.RetrofitInstance
 import kotlinx.coroutines.CoroutineScope
@@ -18,19 +16,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OutletStoreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class OutletStoreFragment : Fragment() {
-    lateinit var adapter: OrdersAdapter
+class DeliveryShopFragment : Fragment() {
+    lateinit var adapter: OrderAdapter_2
     lateinit var recyclerView: RecyclerView
     lateinit var dataList:ArrayList<GetAllOrdersItem>
 
@@ -39,45 +26,38 @@ class OutletStoreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_outlet_store, container, false)
+        return inflater.inflate(R.layout.fragment_delivery_store, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val gotoFilter = view.findViewById<LinearLayout>(R.id.filter_layout)
-        gotoFilter.setOnClickListener {
 
-        }
-        dataList= arrayListOf()
+          dataList= arrayListOf()
         // Find the RecyclerView
-        recyclerView = view.findViewById(R.id.recyclerview1)
+        recyclerView = view.findViewById(R.id.recyclerview123)
 
         // Set layout manager
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         // Set the adapter
-        try {
-            fetchDataAndBindRecyclerview()
-        }
-        catch (e : Exception){
-            Log.e("ExceptionCheck", e.message.toString() )
-        }
+        fetchDataAndBindRecyclerview()
 
 
-
-    }
-    private fun getOutletId():String?{
-        val sharedPref = activity?.getSharedPreferences("EggBucketPrefs", Context.MODE_PRIVATE)
-        return sharedPref?.getString("OUTLET_ID",null)
     }
     fun fetchDataAndBindRecyclerview(){
         CoroutineScope(Dispatchers.IO).launch {
-            val dataList = RetrofitInstance.api.getOrdersByOutletId(getOutletId().toString())
+            val dataList = RetrofitInstance.api.getOrdersByDeliveryId(getUserId().toString())
+            val reversedList = ArrayList(dataList.reversed())
 
             withContext(Dispatchers.Main) {
-                adapter = OrdersAdapter(requireContext(),dataList)
+                adapter = OrderAdapter_2(requireContext(),parentFragmentManager,reversedList)
                 recyclerView.adapter = adapter
             }
         }
+    }
+
+    private fun getUserId():String?{
+        val sharedPref = activity?.getSharedPreferences("EggBucketPrefs", Context.MODE_PRIVATE)
+        return sharedPref?.getString("USER_ID",null)
     }
 }
--- app/src/main/java/com/eggbucket/eggbucket_android/OutletShopFragment.kt
+++ app/src/main/java/com/eggbucket/eggbucket_android/OutletShopFragment.kt
@@ -2,13 +2,14 @@ package com.eggbucket.eggbucket_android
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.eggbucket.eggbucket_android.adapters.OrderAdapter_2
 import com.eggbucket.eggbucket_android.adapters.OrdersAdapter
 import com.eggbucket.eggbucket_android.model.allorders.GetAllOrdersItem
 import com.eggbucket.eggbucket_android.network.RetrofitInstance
@@ -17,8 +18,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class DeliveryStoreFragment : Fragment() {
-    lateinit var adapter: OrderAdapter_2
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [OutletShopFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class OutletShopFragment : Fragment() {
+    lateinit var adapter: OrdersAdapter
     lateinit var recyclerView: RecyclerView
     lateinit var dataList:ArrayList<GetAllOrdersItem>
 
@@ -27,37 +39,46 @@ class DeliveryStoreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_delivery_store, container, false)
+        return inflater.inflate(R.layout.fragment_outlet_store, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val gotoFilter = view.findViewById<LinearLayout>(R.id.filter_layout)
+        gotoFilter.setOnClickListener {
 
-          dataList= arrayListOf()
+        }
+        dataList= arrayListOf()
         // Find the RecyclerView
-        recyclerView = view.findViewById(R.id.recyclerview123)
+        recyclerView = view.findViewById(R.id.recyclerview1)
 
         // Set layout manager
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         // Set the adapter
-        fetchDataAndBindRecyclerview()
+        try {
+            fetchDataAndBindRecyclerview()
+        }
+        catch (e : Exception){
+            Log.e("ExceptionCheck", e.message.toString() )
+        }
 
 
+
+    }
+    private fun getOutletId():String?{
+        val sharedPref = activity?.getSharedPreferences("EggBucketPrefs", Context.MODE_PRIVATE)
+        return sharedPref?.getString("OUTLET_ID",null)
     }
     fun fetchDataAndBindRecyclerview(){
         CoroutineScope(Dispatchers.IO).launch {
-            val dataList = RetrofitInstance.api.getOrdersByDeliveryId(getUserId().toString())
+            val dataList = RetrofitInstance.api.getOrdersByOutletId(getOutletId().toString())
+            val reverseDataList = ArrayList(dataList.reversed())
 
             withContext(Dispatchers.Main) {
-                adapter = OrderAdapter_2(requireContext(),parentFragmentManager,dataList)
+                adapter = OrdersAdapter(requireContext(),reverseDataList)
                 recyclerView.adapter = adapter
             }
         }
-    }
-
-    private fun getUserId():String?{
-        val sharedPref = activity?.getSharedPreferences("EggBucketPrefs", Context.MODE_PRIVATE)
-        return sharedPref?.getString("USER_ID",null)
     }
 }
